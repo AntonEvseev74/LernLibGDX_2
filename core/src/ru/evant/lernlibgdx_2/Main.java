052c38930ea081b4ea7bd191a87f4ba21c578f35
Anton Evseev <58934670+AntonEvseev74@users.noreply.github.com>
2021-11-04 17:04:48 +0500

    Добавлены комментариии ко всему коду

--- core/src/ru/evant/lernlibgdx_2/Main.java
+++ core/src/ru/evant/lernlibgdx_2/Main.java
@@ -28,58 +28,82 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Main extends Game {
+
     private SpriteBatch batch;
+
+    // Данные черепахи
     private Texture turtleTexture;
     private float turtleX;
     private float turtleY;
     private Rectangle turtleRectangle;
+
+    // Данные звезды
     private Texture starfishTexture;
     private float starfishX;
     private float starfishY;
     private Rectangle starfishRectangle;
+
+    // Фон игры
     private Texture oceanTexture;
+
+    // Табличка "Вы победили"
     private Texture winMessageTexture;
+
+    // Состояние игрока (победил или не победил)
     private boolean win;
 
     public void create() {
-        batch = new SpriteBatch();
-        turtleTexture = new Texture(Gdx.files.internal("turtle-1.png"));
-        turtleX = 20;
-        turtleY = 20;
+        batch = new SpriteBatch(); // отрисовщик
+
+        // черепаха
+        turtleTexture = new Texture(Gdx.files.internal("turtle-1.png")); // картинка
+        turtleX = 20; // координата начала отрисовки по оси Х
+        turtleY = 20; // координата начала отрисовки по оси У
         turtleRectangle = new Rectangle(turtleX, turtleY, turtleTexture.getWidth(), turtleTexture.getHeight());
-        starfishTexture = new Texture(Gdx.files.internal("starfish.png"));
-        starfishX = 380;
-        starfishY = 380;
+        // прямоугольник с координатами и размерами черепахи, используется для проверки на столкновения
+
+        // звезда
+        starfishTexture = new Texture(Gdx.files.internal("starfish.png")); // картинка
+        starfishX = 380; // координата начала отрисовки по оси Х
+        starfishY = 380; // координата начала отрисовки по оси У
         starfishRectangle = new Rectangle(starfishX, starfishY, starfishTexture.getWidth(), starfishTexture.getHeight());
-        oceanTexture = new Texture(Gdx.files.internal("water.jpg"));
+        // прямоугольник с координатами и размерами звезды, используется для проверки на столкновения
+
+        // фон игры
+        oceanTexture = new Texture(Gdx.files.internal("water.jpg")); // картнка
+
+        // Табличка "Вы победили"
         winMessageTexture = new Texture(Gdx.files.internal("you-win.jpg"));
+
+        // Состояние игрока - не выиграл
         win = false;
     }
 
     public void render() {
         // проверить ввод данных пользователем
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) turtleX--;
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) turtleX++;
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) turtleY++;
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) turtleY--;
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) turtleX--; // если нажата кнопка влево, уменьшаем координату Х
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) turtleX++; // если нажата кнопка вправо, увеличиваем координату Х
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) turtleY++; // если нажата кнопка вверх, увеличиваем координату У
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) turtleY--; // если нажата кнопка вниз, уменьшаем координату У
 
         // обновить местоположение прямоугольника черепахи
-        turtleRectangle.setPosition(turtleX, turtleY);
+        turtleRectangle.setPosition(turtleX, turtleY); // установить новые координаты для отрисовки черепахи
 
         // проверить условие выигрыша: черепаха должна перекрывать морскую звезду
         if (turtleRectangle.overlaps(starfishRectangle)) win = true;
+        // если прямоугольник черепахи столкнулся с прямоугольником звезды, то установить состояние игрока - выиграл
 
         // очистить экран
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         // нарисовать графику
-        batch.begin();
-        batch.draw(oceanTexture, 0, 0);
-        if (!win) batch.draw(starfishTexture, starfishX, starfishY);
-        batch.draw(turtleTexture, turtleX, turtleY);
-        if (win) batch.draw(winMessageTexture, 220, 180);
-        batch.end();
+        batch.begin(); // начало цикла отрисовки
+        batch.draw(oceanTexture, 0, 0); // нарисовать фон игры по координатам х,у
+        if (!win) batch.draw(starfishTexture, starfishX, starfishY); // если не выиграл нарисовать звезду по координатам х,у
+        batch.draw(turtleTexture, turtleX, turtleY); // нарисовать черепаху по координатам х,у
+        if (win) batch.draw(winMessageTexture, 220, 180); // если выиграл нарисовать табличку "Вы выиграли по координатам х,у
+        batch.end(); // конец цикла отрисовки
     }
 
 }
